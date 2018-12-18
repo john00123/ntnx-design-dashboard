f77894e3e138cc6cb9187fdf0c2a3275e6adc2d1
John Torres <john.torres.hurtado@gmail.com>
2018-12-18 09:32:21 -0800

    firebase

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCX2mp5rLjF0nroDjIo4GtjnDr6CpLkqJ0",
+    authDomain: "ntnx-dteam.firebaseapp.com",
+    databaseURL: "https://ntnx-dteam.firebaseio.com",
+    projectId: "ntnx-dteam",
+    storageBucket: "ntnx-dteam.appspot.com",
+    messagingSenderId: "220244852409"
+  };
+  
+  firebase.initializeApp(config);
+
+  const preObject = $('#object');
+  const dbRefObject = firebase.database.ref().child('object')
+
+  //SYNC object changes
+
+  dbRefObject.on('value', snap => console.log(snap.val()));
