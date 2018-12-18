47d544704c8e6d4869a01e4b8c46d749099c7bfd
John Torres <john.torres.hurtado@gmail.com>
2018-12-18 09:39:26 -0800

    Triggering init function
    Adding function to match the tutorial video.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCX2mp5rLjF0nroDjIo4GtjnDr6CpLkqJ0",
-    authDomain: "ntnx-dteam.firebaseapp.com",
-    databaseURL: "https://ntnx-dteam.firebaseio.com",
-    projectId: "ntnx-dteam",
-    storageBucket: "ntnx-dteam.appspot.com",
-    messagingSenderId: "220244852409"
-  };
 
-  firebase.initializeApp(config);
+(function(){
+  // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyCX2mp5rLjF0nroDjIo4GtjnDr6CpLkqJ0",
+      authDomain: "ntnx-dteam.firebaseapp.com",
+      databaseURL: "https://ntnx-dteam.firebaseio.com",
+      projectId: "ntnx-dteam",
+      storageBucket: "ntnx-dteam.appspot.com",
+      messagingSenderId: "220244852409"
+    };
 
-  const preObject = $('#object');
-  const dbRefObject = firebase.database().ref().child('object')
+    firebase.initializeApp(config);
 
-  //SYNC object changes
+    const preObject = $('#object');
 
-  dbRefObject.on('value', snap => console.log(snap.val()));
+    //create reference
+    const dbRefObject = firebase.database().ref().child('object');
+
+    //SYNC object changes
+
+    dbRefObject.on('value', snap => console.log(snap.val()));
+
+}());
